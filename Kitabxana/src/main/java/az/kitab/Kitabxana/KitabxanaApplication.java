aa3e93286c144b295d0de4f0d8d83fb57d62e98a
SalehHasanguliyev <147057859+SalehHasanguliyev@users.noreply.github.com>
2024-03-21 14:27:05 +0400

    Initial commit

---
+++ Kitabxana/src/main/java/az/kitab/Kitabxana/KitabxanaApplication.java
@@ -0,0 +1,13 @@
+package az.kitab.Kitabxana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitabxanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitabxanaApplication.class, args);
+	}
+
+}
---
+++ Kitabxana/src/test/java/az/kitab/Kitabxana/KitabxanaApplicationTests.java
@@ -0,0 +1,13 @@
+package az.kitab.Kitabxana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitabxanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
